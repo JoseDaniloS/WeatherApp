f7dfad34a78191bca513fd596da765207a6cd237
Jose Danilo <danilojose.1d@gmail.com>
2024-11-18 22:06:04 -0300

    style: text font size change

--- src/components/Clima.jsx
+++ src/components/Clima.jsx
@@ -1,5 +1,9 @@
 import { useState } from "react";
-import { weatherApp, converterKelvinParaCelcius, getCurrentLocationCoords } from "../WeatherAPIs";
+import {
+  weatherApp,
+  converterKelvinParaCelcius,
+  getCurrentLocationCoords,
+} from "../WeatherAPIs";
 
 function Clima() {
   const [cidade, setCidade] = useState("");
@@ -11,17 +15,17 @@ function Clima() {
 
     try {
       const data = await weatherApp(cidade);
-      const coordenadas = await getCurrentLocationCoords(cidade)
+      const coordenadas = await getCurrentLocationCoords(cidade);
       if (data && coordenadas) {
-        const tempCelsius = converterKelvinParaCelcius(data.main.temp); // Converte a temperatura de Kelvin para Celsius
+        const tempCelsius = converterKelvinParaCelcius(data.main.temp); 
         setClima({
           nome: coordenadas[0].name,
           temp: tempCelsius,
           descricao: data.weather[0].description,
-          sensacao: converterKelvinParaCelcius(data.main.feels_like), // Sensação térmica em Celsius
+          sensacao: converterKelvinParaCelcius(data.main.feels_like), 
           umidade: data.main.humidity,
           nomeEstado: coordenadas[0].state,
-          nomePais: coordenadas[0].country
+          nomePais: coordenadas[0].country,
         });
       } else {
         setErro("Cidade não encontrada!");
@@ -32,9 +36,9 @@ function Clima() {
   };
 
   return (
-    <section className="flex gap-10">
+    <section className="flex gap-10 flex-col">
       <div className=" bg-slate-300 rounded-lg p-10">
-        <div>
+        <div className="flex flex-col gap-3">
           <h1 className="font-bold text-[30px]">WeatherClim</h1>
           <form onSubmit={HandleSubmit} className="flex flex-col gap-10">
             <div>
@@ -46,6 +50,7 @@ function Clima() {
                 placeholder="Ex: Brejo Santo"
                 onChange={(e) => setCidade(e.target.value)}
                 className="p-3 rounded-lg w-full"
+                required
               />
             </div>
             <button
@@ -57,29 +62,29 @@ function Clima() {
           </form>
         </div>
       </div>
+
       {/* Exibe os dados do clima se disponíveis */}
       {clima && (
-        <div className="p-10 h-full text-[20px] bg-slate-200 rounded-lg">
-          <h2 className="text-[30px] font-semibold">Clima em {clima.nome} <br/> {clima.nomeEstado} - {clima.nomePais}</h2>
-          <p>Temperatura: {clima.temp} °C</p>
-          <p>
-            Sensação térmica:{" "}
-            {clima.sensacao.charAt(0).toUpperCase() + clima.sensacao.slice(1)}{" "}
-            °C
-          </p>
-          <p>
-            Tempo:{" "}
-            {clima.descricao.charAt(0).toUpperCase() + clima.descricao.slice(1)}
-          </p>
-          <p>Humidade: {clima.umidade}%</p>
+        <div className="p-10 h-full text-[20px] bg-slate-200 rounded-lg space-y-3">
+          <h2 className="text-[35px] font-semibold text-center">
+            {clima.nome}
+            <p className="text-[25px]">{clima.temp} °C</p>
+          </h2>
+          <div>
+            <p>Sensação térmica: {clima.sensacao} °C</p>
+            <p>
+              Tempo:{" "}
+              {clima.descricao.charAt(0).toUpperCase() +
+                clima.descricao.slice(1)}
+            </p>
+            <p>Umidade: {clima.umidade}%</p>
+          </div>
         </div>
       )}
 
       {/* Exibe uma mensagem de erro, se houver */}
       {erro && (
-        <div className="mt-5 p-5 bg-red-200 text-red-700 rounded-lg">
-          {erro}
-        </div>
+        <div className="p-5 bg-red-200 text-red-700 rounded-lg">{erro}</div>
       )}
     </section>
   );
